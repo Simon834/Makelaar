6e2470ca5cc4fad787b8f9cd96d964dae7b4dc97
jeremias-jalil <jjejalil@gmail.com>
2021-09-01 20:28:32 -0300

    personalización de variables email

--- api/email/emailModels/recoveryPass.js
+++ api/email/emailModels/recoveryPass.js
@@ -1,7 +1,15 @@
 const {emailBase} = require('./emailBase')
 
-function recoveryPass(porps){
-    return emailBase(porps)
+function recoveryPass(newPass){
+    const content={
+        title:"Recuperación de contraseña",
+        message:`Ingrese a su cuenta con la siguiente contraseña para poder restaurarla ${newPass || "contraseña nueva"}`,
+        buttonLink:"http://localhost:3000/",
+        buttonText:"Ir a la web",
+        noteMessage:"No comparta esta contraseña"
+    }
+    
+    return emailBase(content)
 }
 
 module.exports = { recoveryPass }
