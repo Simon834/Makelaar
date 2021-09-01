cf8ecc16da64cf17242f961a6ac2da8dc19a4303
marcosvillar456 <72837940+marcosvillar456@users.noreply.github.com>
2021-09-01 11:01:36 -0300

    cambio de funciones a controllers

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,26 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const { User } = require("../models/user");
-const { getUserById } = require("../controllers/usersController")
+const { getUserById, allUsers } = require("../controllers/usersController");
 
 /* GET users listing. */
+router.get("/allUsers", allUsers);
 
-
-router.get("/AllUsers", function (req, res, next) {
-  try {
-    //.findAll() cuando se agregue l
-    const users = User;
-    if (!users.length) {
-      return res.send("No hay usuarios registrados por el momento");
-    } else {
-      return res.send(users);
-    }
-  } catch (err) {
-    return next(err);
-  }
-});
-
-router.get('/:id', getUserById)
-
+router.get("/:id", getUserById);
 
 module.exports = router;
