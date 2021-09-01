45004657169949b6389c9f7c619322a5f167d68f
Fernando <lenin_jaramillo@hotmail.com>
2021-09-02 01:18:51 +0200

    view-base dinamyc

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import "./"
 import ViewBase from "./Views/view-base";
 function App() {
-  return (<div className="App">
-            <ViewBase/>
-          </div>)
+  return (
+            <div className="App">
+              <ViewBase carousel="carousel content" filter="filters"/>
+            </div>
+          )
 }
 
 export default App;
--- client/src/Views/view-base.jsx
+++ client/src/Views/view-base.jsx
@@ -1,18 +1,18 @@
-import "./sViewBase.css";
+import "./styleViewBase.css";
 
-export default function viewBase({filter}) {
+export default function viewBase({topBar, navBar, carousel, filter, content, footBar}) {
     return (
         <div className="base-container">
-            <div className="header-container">
-                <div className="topBar-container"></div>
-                <div className="navBar-container"></div>
-                <div className="carousel-container"></div>
+            <div className={ carousel? "header-container with-carousel" : "header-container"}>
+                <div className="topBar-container">{topBar}</div>
+                <div className="navBar-container">{navBar}</div>
+                {carousel &&<div className="carousel-container">{ carousel }</div>}
             </div>
             <div className="body-container">
-                <div className="filter-container"></div>
-                <div className={filter && "content-container right" : "content-container"}></div>
+                {filter && <div className="filter-container">{filter}</div>}
+                <div className={filter? "content-container right": "content-container"}>{content}</div>
             </div>
-            <div className="footer-container"></div>
+            <div className="footer-container">{footBar}</div>
         </div>
     );
 }
