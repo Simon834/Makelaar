ec2d29097ac24c6024daeb8b5d2292b217f728c1
marcosvillar456 <72837940+marcosvillar456@users.noreply.github.com>
2021-09-01 16:42:54 -0300

    agregado de Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Route } from "react-router";
+
 import UserList from "./Components/UserList/UserList";
 function App() {
   return (
     <div className="App">
-      <Route exact path="/admin/:id" component={UserList}></Route>
+      <Route exact path="/admin/:id" component={UserList} />
     </div>
   );
 }
