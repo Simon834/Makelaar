2a4b530d77124be07f7fffa2c876e9821ca137f6
AlanBVN <alaan567@gmail.com>
2021-09-02 11:19:30 -0300

    agregado el reset de password

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,8 +1,9 @@
 const {User} = require("../db");
+const newPassword = require("../email/emailModels/recoveryPass");
 
 async function getUserById(req, res, next) {
   const userId = req.params.id;
-  console.log('me ejecuto')
+  console.log("me ejecuto");
   try {
     const user = await User.findByPk(userId);
     if (user) {
@@ -28,7 +29,36 @@ async function allUsers(res, req, next) {
     return next(err);
   }
 }
+
+async function resetPassword(req, res, next) {
+  const { email } = req.body;
+  try {
+    //encriptamos pass
+    let password = await bcrypt.hashSync(
+      Math.floor(Math.random() * 1000),
+      Number.parseInt(authConfig.rounds)
+    );
+
+    let user = await User.findOne({ where: { email } });
+    if (user) {
+      user.password = password;
+      await user.save();
+
+      newPassword(recoveryPass(password));
+
+      return res.send(
+        "Tu nueva contraseña ha sido enviada a la direccion de email ingresada."
+      );
+    } else {
+      return res.send("No existe ninguna cuenta creada con ese email.");
+    }
+  } catch (err) {
+    return next(err);
+  }
+}
+
 module.exports = {
   getUserById,
   allUsers,
+  resetPassword,
 };
