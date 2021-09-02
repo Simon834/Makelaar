15d3b9e681923fd236e3cf385417e0930313d203
milagrosniro <niromilagros@gmail.com>
2021-09-02 14:08:08 -0300

    nav-topBar-routelogInBack

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import "./"
 import ViewBase from "./Views/view-base";
 import { Route } from "react-router";
 import UserList from "./Components/UserList/UserList";
+import NavBar from "./Components/NavBar/NavBar";
+import TopBar from "./Components/TopBar/TopBar";
 
 function App() {
   return (
             <div className="App">
-              <ViewBase carousel="carousel content" filter="filters"/>
+              {/* <ViewBase carousel="carousel content" filter="filters"/> */}
               <Route exact path="/admin/:id" component={UserList} />
+              <TopBar/>
+              <NavBar/>
             </div>
           )
 }
