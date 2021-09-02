33e9d57d2ab2d50d2887893d2b7ab19ad62babb9
milagrosniro <niromilagros@gmail.com>
2021-09-02 07:16:11 -0300

    NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,17 @@ import "./App.css";
 import { Route } from "react-router";
 
 import UserList from "./Components/UserList/UserList";
+
+//Prueba Mili NavBar
+import NavBar from "./Components/NavBar/NavBar";
+import TopBar from "./Components/TopBar/TopBar"
+
 function App() {
   return (
     <div className="App">
       <Route exact path="/admin/:id" component={UserList} />
+      <NavBar/>
+      <TopBar/>
     </div>
   );
 }
--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../../images/logo-purple.png'
+import logo from '../../images/logo-color.png'
 
 
 //DAR ESTILOS, ACOMODAR PARA QUE SE VEA BIEN EN LA WEB
 
-export function NavBar(){
+
+// const useStyles = makeStyles((theme) => ({
+//   //para que NavBar se vea debajo de topBar
+//   toolbar: theme.mixins.toolbar
+  
+// }));
+
+export default function NavBar(){
     return(
         <nav>
             <div>
