76629fac71750266fe8a0bb3d42ce27ddab7c4f7
AlanBVN <alaan567@gmail.com>
2021-09-04 13:47:21 -0300

    dialog login

--- client/src/Components/TopBar/TopBar.jsx
+++ client/src/Components/TopBar/TopBar.jsx
@@ -15,158 +15,200 @@ import MenuList from '@material-ui/core/MenuList';
 import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
 import ImageListItemBar from '@material-ui/core/ImageListItemBar';
-import './TopBar.css'
+import { Dialog, DialogContent, DialogActions } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import FormLogin from "../FormLogin/FormLogin";
+import UserRegistrationForm from "../UserRegistrationForm/UserRegistrationFrom";
 
-import InfoIcon from '@material-ui/icons/Info';
+import "./TopBar.css";
+
+import InfoIcon from "@material-ui/icons/Info";
 
 //LISTA IMG PRUEBA
 const itemData = [
-    {
-        url: "http://www.inmobiliariaberrini.com.ar/wp-content/uploads/2021/08/1.0-10-525x328.jpg",
-    },
-    {
-        url: "http://www.inmobiliariaberrini.com.ar/wp-content/uploads/2021/08/1.0-14-525x328.jpg",
-    },
-    {
-        url: "http://www.inmobiliariaberrini.com.ar/wp-content/uploads/2021/08/1.0-13-525x328.jpg",
-    },
+  {
+    url: "http://www.inmobiliariaberrini.com.ar/wp-content/uploads/2021/08/1.0-10-525x328.jpg",
+  },
+  {
+    url: "http://www.inmobiliariaberrini.com.ar/wp-content/uploads/2021/08/1.0-14-525x328.jpg",
+  },
+  {
+    url: "http://www.inmobiliariaberrini.com.ar/wp-content/uploads/2021/08/1.0-13-525x328.jpg",
+  },
 ];
 
-
 const useStyles = makeStyles((theme) => ({
-
-    menuList: {
-        width: 800,
-        height: 1600,
-        display: 'flex',
-        margin: theme.spacing(7, 'auto'),
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-
-    },
-    imgList: {
-        // flexDirection:'row',
-        // justifyContent: 'center',
-        width: 500,
-        height: 450,
-    },
-    imgItem: {
-        // width:20,
-        // height:20,
-    }
-
+  menuList: {
+    width: 800,
+    height: 1600,
+    display: "flex",
+    margin: theme.spacing(7, "auto"),
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+  },
+  imgList: {
+    // flexDirection:'row',
+    // justifyContent: 'center',
+    width: 500,
+    height: 450,
+  },
+  button: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "12px",
+    marginBottom: "20px",
+    marginTop: "12px",
+    padding: "6px",
+    marginLeft: theme.spacing(49),
+  },
 }));
 
 export default function TopBar() {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
 
-    const [open, setOpen] = useState(false);
-    const anchorRef = useRef(null);
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
 
-    const handleToggle = () => {
-        setOpen((prevOpen) => !prevOpen);
-    };
-
-    const handleClose = (event) => {
-        if (anchorRef.current && anchorRef.current.contains(event.target)) {
-            return;
-        }
-
-        setOpen(false);
-    };
-
-    function handleListKeyDown(event) {
-        if (event.key === 'Tab') {
-            event.preventDefault();
-            setOpen(false);
-        }
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
     }
 
-    // return focus to the button when we transitioned from !open -> open
-    const prevOpen = useRef(open);
-    useEffect(() => {
-        if (prevOpen.current === true && open === false) {
-            anchorRef.current.focus();
-        }
+    setOpen(false);
+  };
 
-        prevOpen.current = open;
-    }, [open]);
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
 
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = useRef(open);
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
 
-   
+    prevOpen.current = open;
+  }, [open]);
 
-    return (
+  //ESTO ES DEL DIALOG DEL LOGIN
 
-        <div className="containerTopBar">
-            <div className="containerContact">
-                <IconButton>
-                    <WhatsAppIcon />
-                    <Typography>+549 11456982365</Typography>
-                </IconButton>
+  const [showDialog, setShowDialog] = useState(false);
 
-                <IconButton>
-                    <MailOutlineIcon />
-                    <Typography>makelaar@gmail.com</Typography>
-                </IconButton>
-            </div>
+  const openDialog = () => setShowDialog(true);
+  const closeDialog = () => setShowDialog(false);
 
-            <div className="containerIcons">
-                <Tooltip title="favoritos">
-                    <IconButton ref={anchorRef}
-                        aria-controls={open ? 'menu-list-grow' : undefined}
-                        aria-haspopup="true"
-                        onClick={handleToggle}
-                        arial-label="app" >
-                        <FavoriteBorderIcon />
-                    </IconButton>
-                </Tooltip>
-                <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
-                    {({ TransitionProps, placement }) => (
-                        <Grow
-                            {...TransitionProps}
-                            style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-                        >
-                            <Paper>
+  ////////////////////////////////
 
-                                <ClickAwayListener onClickAway={handleClose}>
+  return (
+    <div className="containerTopBar">
+      <div className="containerContact">
+        <IconButton>
+          <WhatsAppIcon />
+          <Typography>+549 11456982365</Typography>
+        </IconButton>
 
-                                    <MenuList className={classes.menuList}>
-                                        <ImageList rowHeight={180} className={classes.imgList} autoFocusItem={open} onKeyDown={handleListKeyDown}>
-                                            {itemData && itemData.map((item) => (
-                                                <ImageListItem className={classes.imgItem} cols={1} style={{ height: 'auto' }} key={item.url}>
-                                                    <img src={item.url} alt="img" />
-                                                    <ImageListItemBar
-                                                        title="title"
-                                                        subtitle="by: Makelaar"
-                                                        actionIcon={
-                                                            <IconButton aria-label="title" >
-                                                                <InfoIcon />
-                                                            </IconButton>
-                                                        }
-                                                    />
-                                                </ImageListItem>
-                                            ))}
-                                        </ImageList>
+        <IconButton>
+          <MailOutlineIcon />
+          <Typography>makelaar@gmail.com</Typography>
+        </IconButton>
+      </div>
 
-                                    </MenuList>
-                                </ClickAwayListener>
-                            </Paper>
-                        </Grow>
-                    )}
-                </Popper>
+      <div className="containerIcons">
+        <Tooltip title="Favoritos">
+          <IconButton
+            ref={anchorRef}
+            aria-controls={open ? "menu-list-grow" : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+            arial-label="app"
+          >
+            <FavoriteBorderIcon />
+          </IconButton>
+        </Tooltip>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList className={classes.menuList}>
+                    <ImageList
+                      rowHeight={180}
+                      className={classes.imgList}
+                      autoFocusItem={open}
+                      onKeyDown={handleListKeyDown}
+                    >
+                      {itemData &&
+                        itemData.map((item) => (
+                          <ImageListItem
+                            className={classes.imgItem}
+                            cols={1}
+                            style={{ height: "auto" }}
+                            key={item.url}
+                          >
+                            <img src={item.url} alt="img" />
+                            <ImageListItemBar
+                              title="title"
+                              subtitle="by: Makelaar"
+                              actionIcon={
+                                <IconButton aria-label="title">
+                                  <InfoIcon />
+                                </IconButton>
+                              }
+                            />
+                          </ImageListItem>
+                        ))}
+                    </ImageList>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
 
-                <Tooltip title="ingresar">
-                    <IconButton arial-label="app" >
-                        <PermIdentityIcon />
-                    </IconButton>
-                </Tooltip>
-            </div>
-
-        </div>
-
-    )
+        <Tooltip title="Ingresar">
+          <IconButton arial-label="app">
+            <PermIdentityIcon onClick={openDialog} />
+            <Dialog open={showDialog} onClose={closeDialog}>
+              <DialogContent>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  className={classes.button}
+                  onClick={closeDialog}
+                >
+                  X
+                </Button>
+                <FormLogin />
+              </DialogContent>
+            </Dialog>
+          </IconButton>
+        </Tooltip>
+      </div>
+    </div>
+  );
 }
 
