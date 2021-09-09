aed19342f7e8ae948a7f3ac7ea784a737e5bc55b
Fernando <lenin_jaramillo@hotmail.com>
2021-09-09 19:47:49 +0200

    search in process

--- api/controllers/filterControllers.js
+++ api/controllers/filterControllers.js
@@ -1,13 +1,30 @@
 const { Property } = require("../db");
+const { Op } = require('sequelize');
 
 async function filterProperties(req, res, next){
-    { name,
+    let { name,
       area,
       rooms,
       bathrooms,
       types,
       city,
-      neighborhood
-    } = req.
-
+      neighborhood,
+      province,
+      street,
+      description,
+      transaction,
+      condition,
+    } = req.query
+    let filteredProperty = await Property.findAll({
+        where: {
+            [Op.and]: [
+                {
+                    status: "activo",
+                },
+                {
+                    area: {area}
+                }
+            ]
+        }
+    })
 }
